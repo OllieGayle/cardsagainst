a7b00b107d13ff0c9a31e332b4d8fbdeb10b7326
oliwil <olivergayle99@gmail.com>
2020-04-01 12:03:33 +0100

    k

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 app = Flask(__name__)
-socketio = SocketIO(app)
+socketio = SocketIO(app, {'cors_allowed_origins': '*'})
 
 @app.route('/')
 @app.route('/index')
